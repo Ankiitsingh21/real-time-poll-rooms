7c685f56b45b841087b0170e86b3f1dafaccc2ff
Ankit Singh <ankiitsingh21@gmail.com>
2026-02-14 17:51:14 +0530

    ready for deployment

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
+const dns = require('dns');
+
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`✅ MongoDB connected: ${conn.connection.host}`);
+    console.log(`✅ MongoDB connected`);
   } catch (error) {
     console.error(`❌ MongoDB connection error: ${error.message}`);
     process.exit(1);
